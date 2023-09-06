1cbecb61e2bbe1be331134e281a56bff363f6b59
Dustin Gallardo <38537077+Dustin2@users.noreply.github.com>
2023-09-06 15:04:33 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeRd-wo_TnqXXSNyD0LGX_XWVfWwVqOFo",
-  authDomain: "cemento-da437.firebaseapp.com",
-  projectId: "cemento-da437",
-  storageBucket: "cemento-da437.appspot.com",
-  messagingSenderId: "278041417919",
-  appId: "1:278041417919:web:6108c6c13f6cba53ca4e0a",
-  measurementId: "G-KRHZRS1RJV",
+
 };
 
 const app = initializeApp(firebaseConfig);
